6854fcacda76d54ed7d51fca7b18bf501a9b91e2
KelBull <kcrbull@gmail.com>
2017-01-20 13:41:20 -0500

    Initial commit

---
+++ app/src/main/java/kc/terraneo/Action.java
@@ -0,0 +1,8 @@
+package kc.terraneo;
+
+/**
+ * Created by iyaerP on 1/20/2017.
+ */
+
+public abstract class Action {
+}
---
+++ app/src/main/java/kc/terraneo/Temple.java
@@ -0,0 +1,8 @@
+package kc.terraneo;
+
+/**
+ * Created by iyaerP on 1/20/2017.
+ */
+
+class Temple extends BoardObject {
+}
