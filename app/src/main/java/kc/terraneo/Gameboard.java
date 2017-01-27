f5f10935d6cb909e2fcd6fd7908778d937bb6ffd
Kelsey <kcrbull@gmail.com>
2017-01-27 15:10:37 -0500

    implemented Tile class, started gameboard

--- app/src/main/java/kc/terraneo/Gameboard.java
+++ app/src/main/java/kc/terraneo/Gameboard.java
@@ -1,51 +1,45 @@
 package kc.terraneo;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+/**
+ * Created by kcb11010 on 1/27/2017.
+ */
 
-public class Gameboard extends AppCompatActivity {
+public class GameBoard {
+    private int numPlayers;
+    private int radius;
+    private int gridSide;
+    private Tile[][] tiles;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gameboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    /**
+     *
+     */
+    public GameBoard(int _numPlayers) throws IllegalPlayerCountException {
+        switch(_numPlayers){
+            case 2: radius = 3;
+                break;
+            case 3: radius = 3;
+                break;
+            case 4: radius = 4;
+                break;
+            case 5: radius = 5;
+                break;
+            case 6: radius = 5;
+                break;
+            default: throw new IllegalPlayerCountException("Illegal player count "+ _numPlayers +" is not a legal player count");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gameboard, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
-        return super.onOptionsItemSelected(item);
+        gridSide = 2*radius+1;
+        tiles = new Tile[gridSide][gridSide];
+
+        for(int i=0; i< gridSide; i++)
+        {
+
+        }
+    }
+
+    private class IllegalPlayerCountException extends Exception {
+        public IllegalPlayerCountException(String s) {
+            super(s);
+        }
     }
 }
--- app/src/main/java/kc/terraneo/Tile.java
+++ app/src/main/java/kc/terraneo/Tile.java
@@ -1,8 +1,53 @@
 package kc.terraneo;
 
+import android.graphics.Color;
+
 /**
  * Created by iyaerP on 1/20/2017.
  */
 
 class Tile extends BoardObject {
+    private boolean hasPlayer;
+    private boolean isSiphoned;
+    private boolean hasTemple;
+    private int resonance;
+    private int color;
+    private String artPath;
+    private Position position;
+
+
+    /**
+     *
+     */
+    public Tile(int _resonance, int _color, Position p)
+    {
+         hasPlayer = false;
+        hasTemple = false;
+        isSiphoned = false;
+        position = p;
+        color = _color;
+        resonance = _resonance;
+        switch(resonance) {
+            case -1: artPath = "empty";
+                break;
+            case 0: artPath = "calm";
+                break;
+            case 1: artPath = "agitated";
+                break;
+            case 2: artPath = "violent";
+                break;
+        }
+
+        switch(color){
+            case Color.RED: artPath += "fire";
+                break;
+            case Color.BLUE: artPath += "water";
+                break;
+            case Color.YELLOW: artPath += "air";
+                break;
+            case Color.GREEN: artPath += "earth";
+                break;
+            case Color.BLACK: artPath += "source";
+        }
+    }
 }
