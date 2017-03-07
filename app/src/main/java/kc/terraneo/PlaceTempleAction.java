65c2e72139fa59eb4dc9bad818bf8b4a60b9de7c
Kelsey <kcrbull@gmail.com>
2017-03-07 16:44:39 -0500

    added gameWindow

--- app/src/main/java/kc/terraneo/PlaceTempleAction.java
+++
@@ -1,8 +0,0 @@
-package kc.terraneo;
-
-/**
- * Created by kcb11010 on 3/7/2017.
- */
-
-public class PlaceTempleAction extends MajorAction {
-}
--- app/src/main/java/kc/terraneo/UpgradeSource.java
+++ app/src/main/java/kc/terraneo/UpgradeSource.java
@@ -1,17 +1,43 @@
 package kc.terraneo;
 
+import java.util.Collections;
+import java.util.Stack;
+
 /**
  * Created by kcb11010 on 3/7/2017.
  */
 
 public class UpgradeSource extends Source {
-    @Override
-    public void shuffle() {
 
+    private Stack<Upgrade> theUpgrades;
+
+    public UpgradeSource(Upgrade[] Upgrades)
+    {
+        theUpgrades = new Stack<Upgrade>();
+        for(int i=0; i<Upgrades.length; i++)
+        {
+            theUpgrades.push(Upgrades[i]);
+        }
+        shuffle();
     }
 
     @Override
-    public Object pop() {
-        return null;
+    public void shuffle() {
+        Collections.shuffle(theUpgrades);
+    }
+
+    @Override
+    public Upgrade pop() {
+        return theUpgrades.pop();
+    }
+
+
+    public void push(Upgrade t) {
+        theUpgrades.push(t);
+    }
+
+    public Upgrade peek()
+    {
+        return theUpgrades.peek();
     }
 }
