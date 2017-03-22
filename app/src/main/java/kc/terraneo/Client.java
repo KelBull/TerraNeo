e59ba37956a14fe0f4ecdf844b087c0e4149ebe6
Kelsey <kcrbull@gmail.com>
2017-03-22 15:32:01 -0400

    GameWindow now populates data structures for the Tile and Upgrade Sources

--- app/src/main/java/kc/terraneo/Client.java
+++ app/src/main/java/kc/terraneo/Client.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.WindowManager;
 
 public class Client extends AppCompatActivity {
-    GameBoard gBoard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/kc/terraneo/GameWindow.java
+++ app/src/main/java/kc/terraneo/GameWindow.java
@@ -1,20 +1,50 @@
 package kc.terraneo;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import kc.terraneo.R;
+import java.util.ArrayList;
+import java.util.Collections;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+import kc.terraneo.R;
+import kc.terraneo.upgrades.Chaos;
+import kc.terraneo.upgrades.Cities;
+import kc.terraneo.upgrades.Crime;
+import kc.terraneo.upgrades.Death;
+import kc.terraneo.upgrades.Destruction;
+import kc.terraneo.upgrades.Dreams;
+import kc.terraneo.upgrades.Exploration;
+import kc.terraneo.upgrades.Games;
+import kc.terraneo.upgrades.Harvests;
+import kc.terraneo.upgrades.Invention;
+import kc.terraneo.upgrades.Knowledge;
+import kc.terraneo.upgrades.Magic;
+import kc.terraneo.upgrades.Medicine;
+import kc.terraneo.upgrades.Music;
+import kc.terraneo.upgrades.Plague;
+import kc.terraneo.upgrades.Time;
+import kc.terraneo.upgrades.War;
+import kc.terraneo.upgrades.Wealth;
+
 public class GameWindow extends AppCompatActivity {
+    GameBoard gBoard;
+    UpgradeSource uS1;
+    UpgradeSource uS2;
+    UpgradeSource uS3;
+    TileSource tS1;
+    TileSource tS2;
+    TileSource tS3;
+    TileSource tS4;
+    ArrayList<Tile> masterTileList;
+    ArrayList<Upgrade> masterUpgradeList;
+
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -161,5 +191,94 @@ public class GameWindow extends AppCompatActivity {
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    /**
+     * starts the game client up for a new session. This should only ever be run once/game
+     * @param numPlayers the number of players
+     */
+    private void start(int numPlayers)
+    {
+        try {
+            gBoard = new GameBoard(numPlayers);
+        }catch(Exception e){
+            Log.e("error",e.getMessage());
+        }
+        populateTileList();
+        Collections.shuffle(masterTileList);
+        Collections.shuffle(masterUpgradeList);
+
+        Tile[] tiles = new Tile[27];
+        Tile[] tiles2 = new Tile[27];
+        Tile[] tiles3 = new Tile[27];
+        Tile[] tiles4 = new Tile[27];
+        for(int i=0; i<27; i++)
+        {
+            tiles[i] = masterTileList.get(i);
+            tiles2[i] = masterTileList.get(i+27);
+            tiles3[i] = masterTileList.get(i+54);
+            tiles4[i] = masterTileList.get(i+81);
+        }
+        tS1 = new TileSource(tiles);
+        tS2 = new TileSource(tiles2);
+        tS3 = new TileSource(tiles3);
+        tS4 = new TileSource(tiles4);
+
+        Upgrade[] u1 = new Upgrade[6];
+        Upgrade[] u2 = new Upgrade[6];
+        Upgrade[] u3 = new Upgrade[6];
+
+        for(int i=0; i<6; i++)
+        {
+            u1[i] = masterUpgradeList.get(i);
+            u2[i] = masterUpgradeList.get(i+6);
+            u3[i] = masterUpgradeList.get(i+12);
+        }
+        uS1 = new UpgradeSource(u1);
+        uS2 = new UpgradeSource(u2);
+        uS3 = new UpgradeSource(u3);
+
+        for(int i=0; i<numPlayers; i++)
+        {
+            //TODO: add a popup that asks each player to pick a god
+        }
+    }
+
+    /**
+     * fills the masterTileList with all the tiles for the game
+     */
+    private void populateTileList()
+    {
+        masterTileList = new ArrayList<Tile>();
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 1; j<4; j++)
+            {
+                masterTileList.add(new Tile(j, Color.RED, new OffBoardPosition()));
+                masterTileList.add(new Tile(j, Color.GREEN, new OffBoardPosition()));
+                masterTileList.add(new Tile(j, Color.YELLOW, new OffBoardPosition()));
+                masterTileList.add(new Tile(j, Color.BLUE, new OffBoardPosition()));
+            }
+        }
+
+        masterUpgradeList = new ArrayList<Upgrade>();
+        masterUpgradeList.add(new Chaos());
+        masterUpgradeList.add(new Cities());
+        masterUpgradeList.add(new Crime());
+        masterUpgradeList.add(new Death());
+        masterUpgradeList.add(new Destruction());
+        masterUpgradeList.add(new Dreams());
+        masterUpgradeList.add(new Exploration());
+        masterUpgradeList.add(new Games());
+        masterUpgradeList.add(new Harvests());
+        masterUpgradeList.add(new Invention());
+        masterUpgradeList.add(new Knowledge());
+        masterUpgradeList.add(new Magic());
+        masterUpgradeList.add(new Medicine());
+        masterUpgradeList.add(new Music());
+        masterUpgradeList.add(new Plague());
+        masterUpgradeList.add(new Time());
+        masterUpgradeList.add(new War());
+        masterUpgradeList.add(new Wealth());
     }
 }
